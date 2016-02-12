c30519933615632b379c49c8a1052427837913c5
yiha051975 <sheldoncslee@gmail.com>
2016-02-11 22:47:44 -0600

    make node js to add uploads folder if it doesn't exists

--- ReduxTutorial/server.js
+++ ReduxTutorial/server.js
@@ -7,6 +7,12 @@ var fs = require("fs");
 var bodyParser = require('body-parser');
 var multiparty = require('multiparty');
 
+fs.stat('./uploads', function(err, stats) {
+    if (err) {
+        fs.mkdir('./uploads')
+    }
+});
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
