97a6f54d7d10dc1b581f861747b1f38ed8018fff
yiha051975 <sheldoncslee@gmail.com>
2017-03-06 09:18:52 -0600

    update on react router and form post

---
+++ ReactRouterAndFormPostReduxSimpleStarter/src/components/posts_index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Sheldon Lee on 3/6/2017.
+ */
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+    componentWillMount() {
+        console.log('this would be a good time to call an action creator to fetch posts.')
+    }
+
+    render() {
+        return (
+            <div>List of blog posts</div>
+        );
+    }
+}
+
+export default PostsIndex;
---
+++ ReactRouterAndFormPostReduxSimpleStarter/src/routes.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Sheldon Lee on 3/6/2017.
+ */
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+import PostsIndex from './components/posts_index';
+
+export default(
+    <Route path="/" component={App}>
+        <IndexRoute component={PostsIndex} />
+    </Route>
+);
