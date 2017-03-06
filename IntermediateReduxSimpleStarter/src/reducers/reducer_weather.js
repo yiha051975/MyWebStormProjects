cbfc445e35285314b8d83cea741fd0ee0fa26ee5
yiha051975 <sheldoncslee@gmail.com>
2017-03-06 07:41:16 -0600

    add new project for react router and redux form

--- IntermediateReduxSimpleStarter/src/reducers/reducer_weather.js
+++ IntermediateReduxSimpleStarter/src/reducers/reducer_weather.js
@@ -3,6 +3,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
     switch (action.type) {
         case FETCH_WEATHER:
+            // similar to state.concat(action.payload.data)
             return [action.payload.data, ...state];
     }
     return state;
