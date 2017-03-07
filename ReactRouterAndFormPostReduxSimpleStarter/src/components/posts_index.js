da29681bd923c46abe70fa1c2e3e07c46ec304af
yiha051975 <sheldoncslee@gmail.com>
2017-03-07 10:31:45 -0600

    Finish up tutorial for React Router and Redux-Form.

--- ReactRouterAndFormPostReduxSimpleStarter/src/components/posts_index.js
+++ ReactRouterAndFormPostReduxSimpleStarter/src/components/posts_index.js
@@ -2,17 +2,57 @@
  * Created by Sheldon Lee on 3/6/2017.
  */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+/*import { bindActionCreators } from 'redux';*/
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
     componentWillMount() {
-        console.log('this would be a good time to call an action creator to fetch posts.')
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        return this.props.posts.map(function(post) {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    <Link to={"posts/" + post.id}>
+                        <span className="pull-xs-right">{post.categories}</span>
+                        <strong>{post.title}</strong>
+                    </Link>
+                </li>
+            )
+        })
     }
 
     render() {
         return (
-            <div>List of blog posts</div>
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                        Add a Post
+                    </Link>
+                </div>
+                List of blog posts
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
+            </div>
         );
     }
 }
 
-export default PostsIndex;
+/*function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);*/
+
+function mapStateToProps(state) {
+    return {
+        posts: state.posts.all
+    }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
---
+++ ReactRouterAndFormPostReduxSimpleStarter/src/components/posts_new.js
@@ -0,0 +1,87 @@
+/**
+ * Created by Sheldon Lee on 3/6/2017.
+ */
+import React, { Component, PropTypes } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
+import { Link } from 'react-router';
+
+class PostsNew extends Component {
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    onSubmit(props) {
+        this.props.createPost(props)
+            .then(() => {
+                // blog post has been created, navigate user to the index
+                // We navigate by calling this.context.rounter.push with
+                // new path to navigate to.
+                this.context.router.push('/');
+            });
+    }
+
+    render() {
+        // const handleSubmit = this.props.handleSubmit;
+        // const title = this.props.fields.title
+        // const categories = this.props.fields.categories
+        // const content = this.props.fields.content
+        const { fields: { title, categories, content }, handleSubmit } = this.props;
+        // console.log(title);
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <h3>Create A New Post</h3>
+                <div className={`form-group${title.touched && title.invalid ? ' has-danger' : ''}`}>
+                    <label>Title</label>
+                    <input type="text" className="form-control" {...title} />
+                    <div className="text-help">
+                        {title.touched ? title.error : ''}
+                    </div>
+                </div>
+                <div className={`form-group${categories.touched && categories.invalid ? ' has-danger' : ''}`}>
+                    <label>Category</label>
+                    <input type="text" className="form-control" {...categories} />
+                    <div className="text-help">
+                        {categories.touched ? categories.error : ''}
+                    </div>
+                </div>
+                <div className={`form-group${content.touched && content.invalid ? ' has-danger' : ''}`}>
+                    <label>Content</label>
+                    <textarea className="form-control" {...content} />
+                    <div className="text-help">
+                        {content.touched ? content.error : ''}
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
+            </form>
+        );
+    }
+}
+
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = 'Enter a username';
+    }
+
+    if (!values.categories) {
+        errors.categories = 'Enter categories';
+    }
+
+    if (!values.content) {
+        errors.content = 'Enter some content';
+    }
+
+    return errors;
+}
+
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1stt is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+export default reduxForm({
+    form: 'PostsNewForm',
+    fields: ['title', 'categories', 'content'],
+    validate
+}, null, { createPost })(PostsNew);
