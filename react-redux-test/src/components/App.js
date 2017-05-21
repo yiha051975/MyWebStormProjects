c962a3649fa639dad8785bc5233e7930dd88096f
Sheldon Lee <sheldoncslee@gmail.com>
2017-05-21 11:03:02 -0500

    Fixed page animation

--- react-redux-test/src/components/App.js
+++ react-redux-test/src/components/App.js
@@ -10,30 +10,36 @@ import {LANDING_PAGE, FORM} from '../utils/routes';
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
 class App extends Component {
+    getRoutes({location}) {
+        return (
+            <CSSTransitionGroup
+                transitionName="fade"
+                transitionEnterTimeout={500}
+                transitionLeaveTimeout={500}
+                component="div"
+                className="main-content-container"
+            >
+                <Switch key={location.key} location={location}>
+                    <Route exact path={LANDING_PAGE} component={LandingPage}/>
+                    <Route path={FORM} component={NewPost}/>
+                    <Route component={ErrorPage}/>
+                </Switch>
+            </CSSTransitionGroup>
+        );
+    }
+
     render() {
         return (
             <Router basename="/react-redux-test">
                 <div className="App">
                     <div className="App-header">
-                        <img src={logo} className="App-logo" alt="logo"/>
+                        <img src={logo} className="App-logo" alt="react logo"/>
                         <h2>Welcome to React</h2>
                     </div>
                     <p className="App-intro">
                         To get started, edit <code>src/App.js</code> and save to reload.
                     </p>
-                    <Route render={({location}) =>
-                        <div className="main-content-container">
-                            <CSSTransitionGroup transitionName="fade"
-                                            transitionEnterTimeout={500}
-                                            transitionLeaveTimeout={500}>
-                                <Switch key={location.key}>
-                                    <Route exact path={LANDING_PAGE} component={LandingPage}/>
-                                    <Route path={FORM} component={NewPost}/>
-                                    <Route component={ErrorPage}/>
-                                </Switch>
-                            </CSSTransitionGroup>
-                        </div>
-                    } />
+                    <Route render={this.getRoutes} />
                     <RouterRedirect/>
                 </div>
             </Router>
