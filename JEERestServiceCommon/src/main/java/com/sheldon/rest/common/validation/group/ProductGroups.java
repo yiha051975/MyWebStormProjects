367f27796dc75b2727246c1e45fa1709ab6dd0ac
Sheldon Lee <sheldoncslee@gmail.com>
2017-06-22 18:56:05 -0500

    remove java project

--- JEERestServiceCommon/src/main/java/com/sheldon/rest/common/validation/group/ProductGroups.java
+++
@@ -1,9 +0,0 @@
-package com.sheldon.rest.common.validation.group;
-
-/**
- * Created by Sheld on 6/21/2017.
- */
-public interface ProductGroups {
-    interface AddProductGroup {}
-    interface DeleteProductGroup {}
-}
